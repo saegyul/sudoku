9c3a8e3916d9667c291097410df542fbaddf8751
YoungSang Yun <saegyul@gmail.com>
2020-06-01 16:54:31 -0700

    beta version without help function when solving puzzle

--- gui.py
+++ gui.py
@@ -12,11 +12,16 @@ class SudokuWindow(QWidget):
         self.ui.setupUi(self)
         self.ui.PB_gen_puzzle.clicked.connect(self.gen_puzzle)
         self.ui.PB_solve_puzzle.clicked.connect(self.solve_puzzle)
+        self.ui.PB_reset_table.clicked.connect(self.resetTable)
 
         table = self.ui.tableWidget
         for i in range(9):
             for j in range(9):
-                table.setItem(i,j,QTableWidgetItem())
+                item = QTableWidgetItem()
+                if ((i//3 + j//3)%2):
+#                    item.setBackground(QtCore.Qt.lightGray)
+                    item.setBackground(QtGui.QColor(220,220,220,127))
+                table.setItem(i,j,item)
 
         pallet = QtGui.QPalette()
         pallet.setColor(QtGui.QPalette.Text, QtCore.Qt.red)
@@ -30,6 +35,7 @@ class SudokuWindow(QWidget):
         solver.generate_puzzle(b1.bd)
 #        b1.display(stop=True)
 
+        self.resetTable()
         table = self.ui.tableWidget
         for i in range(9):
             for j in range(9):
@@ -44,3 +50,48 @@ class SudokuWindow(QWidget):
 
     def solve_puzzle(self):
         print("solve puzzle")
+        b1 = Board()
+        table = self.ui.tableWidget
+        data = []
+        for i in range(9):
+            s = ""
+            for j in range(9):
+                n = table.item(i,j).text()
+                print(f'({i},{j}) = {n}')
+                if not n : s += '0'
+                else: s+= n
+            data.append(s)
+
+        b1.fillFromString(data)
+#        b1.display(stop=True)
+
+        ans = []
+        solver.find_solution(b1,ans)
+
+        if len(ans) != 0: 
+            Board(ans[0]).show()
+            self.fillTable(ans[0])
+        else:
+            print("no solution")
+
+    def resetTable(self):
+        table = self.ui.tableWidget
+        for i in range(9):
+            for j in range(9):
+                table.item(i,j).setText("")
+                table.item(i,j).setTextAlignment(QtCore.Qt.AlignCenter)
+                table.item(i,j).setForeground(QtCore.Qt.green)
+        table.viewport().update()
+
+
+    def fillTable(self, bd):
+        table = self.ui.tableWidget
+        for i in range(9):
+            for j in range(9):
+                if bd[i][j]:
+#                    print(f'({i},{j}) = {bd[i][j]}')
+                    table.item(i,j).setText(str(bd[i][j]))
+                    table.item(i,j).setTextAlignment(QtCore.Qt.AlignCenter)
+                    table.item(i,j).setForeground(QtCore.Qt.green)
+        table.viewport().update()
+
--- sudoku_ui.py
+++ sudoku_ui.py
@@ -2,13 +2,11 @@
 
 # Form implementation generated from reading ui file 'sudoku.ui'
 #
-# Created by: PyQt5 UI code generator 5.12.3
+# Created by: PyQt5 UI code generator 5.9.2
 #
 # WARNING! All changes made in this file will be lost!
 
-
 from PyQt5 import QtCore, QtGui, QtWidgets
-
 
 class Ui_Form(object):
     def setupUi(self, Form):
@@ -17,11 +15,28 @@ class Ui_Form(object):
         self.tableWidget = QtWidgets.QTableWidget(Form)
         self.tableWidget.setGeometry(QtCore.QRect(20, 30, 431, 431))
         self.tableWidget.setAutoScroll(False)
-        self.tableWidget.setAlternatingRowColors(True)
+        self.tableWidget.setAlternatingRowColors(False)
         self.tableWidget.setTextElideMode(QtCore.Qt.ElideMiddle)
         self.tableWidget.setRowCount(9)
         self.tableWidget.setColumnCount(9)
         self.tableWidget.setObjectName("tableWidget")
+        # item = QtWidgets.QTableWidgetItem()
+        # item.setTextAlignment(QtCore.Qt.AlignCenter)
+        # font = QtGui.QFont()
+        # font.setPointSize(44)
+        # font.setBold(True)
+        # font.setWeight(75)
+        # item.setFont(font)
+        # brush = QtGui.QBrush(QtGui.QColor(220, 220, 220))
+        # brush.setStyle(QtCore.Qt.NoBrush)
+        # item.setBackground(brush)
+        # item.setFlags(QtCore.Qt.ItemIsSelectable|QtCore.Qt.ItemIsEditable|QtCore.Qt.ItemIsDragEnabled|QtCore.Qt.ItemIsUserCheckable|QtCore.Qt.ItemIsEnabled)
+        # self.tableWidget.setItem(0, 0, item)
+        # item = QtWidgets.QTableWidgetItem()
+        # brush = QtGui.QBrush(QtGui.QColor(197, 197, 197))
+        # brush.setStyle(QtCore.Qt.NoBrush)
+        # item.setBackground(brush)
+        # self.tableWidget.setItem(0, 1, item)
         self.tableWidget.horizontalHeader().setVisible(False)
         self.tableWidget.horizontalHeader().setDefaultSectionSize(48)
         self.tableWidget.horizontalHeader().setHighlightSections(False)
@@ -45,6 +60,9 @@ class Ui_Form(object):
         self.PB_exit = QtWidgets.QPushButton(self.verticalLayoutWidget)
         self.PB_exit.setObjectName("PB_exit")
         self.verticalLayout.addWidget(self.PB_exit)
+        self.PB_reset_table = QtWidgets.QPushButton(Form)
+        self.PB_reset_table.setGeometry(QtCore.QRect(200, 490, 113, 41))
+        self.PB_reset_table.setObjectName("PB_reset_table")
 
         self.retranslateUi(Form)
         self.PB_exit.clicked.connect(Form.close)
@@ -53,6 +71,11 @@ class Ui_Form(object):
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Form"))
+        __sortingEnabled = self.tableWidget.isSortingEnabled()
+        self.tableWidget.setSortingEnabled(False)
+        self.tableWidget.setSortingEnabled(__sortingEnabled)
         self.PB_gen_puzzle.setText(_translate("Form", "Generate Puzzle"))
         self.PB_solve_puzzle.setText(_translate("Form", "Solve Puzzle"))
         self.PB_exit.setText(_translate("Form", "Exit"))
+        self.PB_reset_table.setText(_translate("Form", "Reset"))
+
